90c7192c0c52a47d35a2c624fe4b20492b0d6d5e
creitions <abc13839467626@dingtalk.com>
2025-07-21 20:45:38 +0800

    更新 index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,13 +1,13 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = '一小家'
+export const siteShortName = '一小家'
+export const siteDescription = '记录一切回忆，知识和瞎想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/creitions/nolebase-template'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
