8faffe86bf140fd12fbdb3253e1ef2e598141f27
creitions <abc13839467626@dingtalk.com>
2025-07-24 11:38:20 +0800

    更新 config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -209,7 +209,7 @@ export default defineConfig({
     nav: [
       { text: '主页', link: '/' },
       { text: '注意事项', link: '/笔记/' },
-      { text: '目录', link: '/笔记/目录' },
+      { text: '目录', link: '/笔记/目录/' },
       { text: '最近更新', link: '/toc' },
     ],
     sidebar,
